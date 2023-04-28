01db05669d97e0d87025a5a26f6bd4d463658e59
jessicavadillo <160356@mail.unacar.mx>
2023-04-27 18:47:12 -0600

    Create Eje1.js
    EJERCICIO 1 ¿HICISTE TU TAREA?

---
+++ Eje1.js
@@ -0,0 +1,10 @@
+var tarea= prompt("Hiciste tu tarea? ")
+    if (tarea==="si"){
+        console.log("¡Bien hecho!")
+    }
+    else if(tarea==="no"){
+        console.log("No te rindas, ¡empieza ya mismo!")
+    }
+    else{
+        console.log("Error, no pusiste una respuesta correcta")
+    }
